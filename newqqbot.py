336844b343c47f8512281580c1fc065317085203
faryek <ermak1871@gmail.com>
2023-03-01 19:32:32 +0500

    New feature
    Added translation to english with bot

--- newqqbot.py
+++ newqqbot.py
@@ -2,6 +2,7 @@ import telebot
 import os
 from dotenv import load_dotenv
 import requests
+from deep_translator import GoogleTranslator
 
 load_dotenv()
 
@@ -10,14 +11,33 @@ bot = telebot.TeleBot(os.getenv("TELEGRAM_TOKEN"))
 info = bot.get_updates()
 print(info)
 
+flag1 = False
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, 'Добро пожаловать в Антейку. Список моих команд доступен по команде /help.')
 
+@bot.message_handler(commands=['ten'])
+def set_translate(message):
+    global flag1
+    flag1 = True
+    bot.reply_to(message, 'Перевожу ваши сообщения на английский язык. Для прекращения перевода используйте - /stopt.')
+
+@bot.message_handler(commands=['stopt'])
+def send_welcome(message):
+    global flag1
+    flag1 = False
+    bot.reply_to(message, 'Перевод отключён.')
+
+
 @bot.message_handler(commands=['help'])
 def send_help(message):
-    bot.send_message(message.chat.id, 'Добро пожаловать в Антейку')
+    bot.send_message(message.chat.id, '''Список моих команд:
+/start - Приветствие
+/help - Помощь
+/ten - Перевод ваших сообщений на английский язык
+/stopt - Отключает перевод сообщений''')
 
 
 
@@ -26,10 +46,28 @@ def send_help(message):
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == "@QqBig_bot Привет":
-        bot.send_message(message.chat.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "@QqBig_bot Переверни меня":
-        bot.send_message(message.chat.id, message.text[::-1])
+    if message.chat.type == 'supergroup':
+        if flag1:
+            bot.reply_to(message, GoogleTranslator(source='auto', target='en').translate(message.text))
+        elif message.text == "@QqBig_bot Привет":
+            bot.send_message(message.chat.id, "Привет, чем я могу тебе помочь?")
+        elif message.text == "@QqBig_bot Переверни меня":
+            bot.send_message(message.chat.id, message.text[:-len(message.text)+len('QqBig_bot'):-1])
+        elif message.text == "@QqBig_bot Скок паркуришь?":
+            bot.send_message(message.chat.id, 'А ты кто такой собственно, чтобы спрашивать?')
+            
+    elif message.chat.type == 'private':
+        if flag1:
+            bot.reply_to(message, GoogleTranslator(source='auto', target='en').translate(message.text))
+        elif message.text == "Привет":
+            bot.send_message(message.chat.id, "Привет, чем я могу тебе помочь?")
+        elif message.text == "Переверни меня":
+            bot.send_message(message.chat.id, message.text[::-1])
+        elif message.text == "Скок паркуришь?":
+            bot.send_message(message.chat.id, 'А ты кто такой собственно, чтобы спрашивать?')
+        
+        
+        
 
 
 
